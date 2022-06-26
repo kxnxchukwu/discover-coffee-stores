7f112a75424195a9478d29aff3fdca3d42dde93a
Kenechukwu <kenechukwu.ajufo@gmail.com>
2022-06-26 15:37:09 +0100

    Fix API call error

--- pages/api/getCoffeeStoresByLocation.js
+++ pages/api/getCoffeeStoresByLocation.js
@@ -7,7 +7,7 @@ const getCoffeeStoresByLocation = async (req, res) => {
     res.status(200);
     res.json(response);
   } catch (err) {
-    console.error("There is an error", err);
+    console.error("There is an error", JSON.stringify(err));
     res.status(500);
     res.json({ message: "Oh no! Something went wrong", err });
   }
